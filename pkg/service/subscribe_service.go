572dfb50264a320da7eece57ed8706745fe56b56
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2020-04-23 09:31:38 +0900

    update sample

--- pkg/service/subscribe_service.go
+++ pkg/service/subscribe_service.go
@@ -2,14 +2,38 @@ package service
 
 import (
 	"context"
-	// "os"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/google/go-github/v30/github"
-	// "gopkg.in/src-d/go-git.v4"
-	// "gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 
 	"github.com/ryota-sakamoto/github-apps-substrate/pkg/model/commit"
+	"github.com/ryota-sakamoto/github-apps-substrate/pkg/model/pull"
 	"github.com/ryota-sakamoto/github-apps-substrate/pkg/repository"
+)
+
+type repositoryDetail struct {
+	owner    string
+	name     string
+	cloneURL string
+}
+
+var (
+	appRepositoryURL repositoryDetail = repositoryDetail{
+		owner:    "ryota-sakamoto",
+		name:     "app-repository",
+		cloneURL: "https://github.com/ryota-sakamoto/app-repository.git",
+	}
+	manifestRepositoryURL repositoryDetail = repositoryDetail{
+		owner:    "ryota-sakamoto",
+		name:     "manifest-repository",
+		cloneURL: "https://github.com/ryota-sakamoto/manifest-repository.git",
+	}
 )
 
 type SubscribeService interface {
@@ -31,10 +55,15 @@ func (s subscribeService) SubscribePush(event *github.PushEvent) error {
 		return nil
 	}
 
+	if event.Repo.Owner.GetName() != appRepositoryURL.owner || event.Repo.GetName() != appRepositoryURL.name {
+		log.Printf("push from %s\n", event.Repo.GetURL())
+		return nil
+	}
+
 	err := s.repositoryRepository.UpdateCommitStatus(context.TODO(), event.Installation.GetID(), commit.UpdateStatus{
 		CommitID:    event.HeadCommit.GetID(),
-		OwnerName:   event.Repo.Owner.GetName(),
-		RepoName:    event.Repo.GetName(),
+		OwnerName:   appRepositoryURL.owner,
+		RepoName:    appRepositoryURL.name,
 		Label:       "GitHub Apps",
 		Description: "wait",
 		Status:      commit.COMMIT_STATUS_PENDING,
@@ -43,42 +72,68 @@ func (s subscribeService) SubscribePush(event *github.PushEvent) error {
 		return err
 	}
 
-	// repo, path, err := s.repositoryRepository.CloneRepository(context.TODO(), event.Installation.GetID(), event.Repo.GetCloneURL(), event.GetRef())
-	// if err != nil {
-	// 	return err
-	// }
-	// defer os.RemoveAll(path)
+	repo, path, err := s.repositoryRepository.CloneRepository(context.TODO(), event.Installation.GetID(), manifestRepositoryURL.cloneURL, event.GetRef())
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(path)
 
-	// current := plumbing.ReferenceName(event.GetRef()).Short()
-	// log.Println("current:", current)
+	current := plumbing.ReferenceName(event.GetRef()).Short()
+	log.Println("current:", current)
 
-	// if current != "develop" {
-	// 	err := s.repositoryRepository.UpdateCommitStatus(context.TODO(), event.Installation.GetID(), commit.UpdateStatus{
-	// 		CommitID:    event.HeadCommit.GetID(),
-	// 		OwnerName:   event.Repo.Owner.GetName(),
-	// 		RepoName:    event.Repo.GetName(),
-	// 		Label:       "GitHub Apps",
-	// 		Description: "ok",
-	// 		Status:      commit.COMMIT_STATUS_SUCCESS,
-	// 	})
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
+	if current == "develop" {
+		w, _ := repo.Worktree()
+		newBranch := fmt.Sprintf("update-develop-%d", time.Now().Unix())
+		if err := w.Checkout(&git.CheckoutOptions{
+			Create: true,
+			Branch: plumbing.NewBranchReferenceName(newBranch),
+		}); err != nil {
+			return err
+		}
 
-	// log.Println(path)
-	// w, _ := repo.Worktree()
-	// if err := w.Checkout(&git.CheckoutOptions{
-	// 	Create: true,
-	// 	Branch: plumbing.NewBranchReferenceName("feature/123456"),
-	// }); err != nil {
-	// 	return err
-	// }
+		f, err := os.Create(path + "/commit-hash")
+		if err != nil {
+			return err
+		}
+		f.Write([]byte(event.HeadCommit.GetID()))
+		f.Close()
 
-	// if err := repo.Push(&git.PushOptions{}); err != nil {
-	// 	log.Printf("%+v\n", err)
-	// 	return err
-	// }
+		w.Add(".")
+		w.Commit("update commit hash", &git.CommitOptions{
+			Author: &object.Signature{
+				Name:  "github-actions[bot]",
+				Email: "41898282+github-actions[bot]@users.noreply.github.com",
+			},
+		})
+
+		if err := repo.Push(&git.PushOptions{}); err != nil {
+			log.Printf("%+v\n", err)
+			return err
+		}
+
+		err = s.repositoryRepository.CreatePullRequest(context.TODO(), event.Installation.GetID(), pull.Request{
+			Title: "update commit hash",
+			Base:  "develop",
+			Head:  newBranch,
+			Body:  "update",
+		})
+		if err != nil {
+			log.Printf("%+v\n", err)
+			return err
+		}
+	}
+
+	err = s.repositoryRepository.UpdateCommitStatus(context.TODO(), event.Installation.GetID(), commit.UpdateStatus{
+		CommitID:    event.HeadCommit.GetID(),
+		OwnerName:   appRepositoryURL.owner,
+		RepoName:    appRepositoryURL.name,
+		Label:       "GitHub Apps",
+		Description: "ok",
+		Status:      commit.COMMIT_STATUS_SUCCESS,
+	})
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
